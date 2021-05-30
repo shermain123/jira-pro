a42118794bc49374956c29242c5426a9f121d9df
itpersion001 <442695679@qq.com>
2021-05-30 12:23:03 +0800

    context

---
+++ src/context/auth-context.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+const AuthContext = React.createContext(undefined)
+AuthContext.displayName = "AuthContext"
+
+export const AuthProvider = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
